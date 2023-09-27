3a55cf1ce7029e4ea8853da1426e12bae6054ddf
muhammadimam155 <imamaliatari@gmail.com>
2023-09-27 23:34:00 +0500

    JS

---
+++ app.js
@@ -0,0 +1,36 @@
+var username = prompt("Your Name", "Imam")
+var age = prompt("Your Age", "15")
+var gender = prompt("Your Gender", "man")
+var email = prompt("Your Email", "imamaliat......")
+var city = prompt("Your City", "jhuddo")
+var country = prompt("Your Country", "pakistan")
+
+
+
+
+console.log(username)
+console.log(age)
+console.log(gender)
+console.log(email)
+console.log(city)
+console.log(country)
+
+
+
+
+alert("you said your name is "+ username +" ")
+alert("you said your age is "+ age +" ")
+alert("you said your gender is "+ gender +" ")
+alert("you said your email is "+ email +" ")
+alert("you said your city is "+ city +" ")
+alert("you said your country is "+ country +" \nRIGHT")
+
+
+
+
+document.write("your name is "+ username +"   ")
+ document.write(" your age is "+ age +"  ")
+document.write("your gender is "+ gender +"  ")
+document.write("your email is "+ email +"  ")
+document.write("your city is "+ city +"  ")
+document.write("your country is "+ country +" ")
